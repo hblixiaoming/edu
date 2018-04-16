ad9908ba6da9ff315bae6c7808299383b01b1752
lixiaoming <lixiaoming@weshare.com.cn>
2018-04-16 18:01:55 +0800

    change package

---
+++ src/main/java/com/lxm/netty/io/base/TimeClient.java
@@ -0,0 +1,38 @@
+package com.lxm.netty.io.base;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class TimeClient {
+    public void connect(int port,String host)throws Exception{
+        EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(eventLoopGroup)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY,true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new TimeClientHandler());
+                        }
+                    });
+
+            ChannelFuture f = b.connect(host,port).sync();
+            f.channel().closeFuture().sync();
+
+        }finally {
+            eventLoopGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args)throws Exception{
+        new TimeClient().connect(8080,"127.0.0.1");
+    }
+}
