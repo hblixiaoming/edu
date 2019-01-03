d47d412bae1fe5a83e67862f21fc235ead40d1d2
lixiaoming <lixiaoming@renrenche.com>
2019-01-03 11:48:15 +0800

    测试代码

--- src/main/java/com/lxm/netty/io/rpc/consumer/TestRpc.java
+++ src/main/java/com/lxm/netty/io/rpc/consumer/TestRpc.java
@@ -8,7 +8,9 @@ public class TestRpc {
     public static void main(String[] args) {
         RemoteServer server = (RemoteServer) Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(),
                 new Class[]{RemoteServer.class}, new RpcInvocationHandler());
-        String result = server.hello("zhangsan");
+        System.out.println("before invoke remote service");
+        String result = server.hello("world");
         System.out.println(result);
+        System.out.println("after invoke remote service");
     }
 }
--- src/main/java/com/lxm/netty/io/rpc/provider/RemoteServerImpl.java
+++ src/main/java/com/lxm/netty/io/rpc/provider/RemoteServerImpl.java
@@ -5,6 +5,11 @@ import com.lxm.netty.io.rpc.publicinterface.RemoteServer;
 public class RemoteServerImpl implements RemoteServer {
     @Override
     public String hello(String user) {
+        try {
+            Thread.sleep(5000);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
         return "hello " + user;
     }
 }
