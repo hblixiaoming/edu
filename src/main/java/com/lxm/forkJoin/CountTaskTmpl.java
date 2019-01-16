8141c8a7308f5776369e8640037ba15ab9ca3817
lixiaoming <lixiaoming@renrenche.com>
2019-01-16 10:28:58 +0800

    nety hettp

--- src/main/java/com/lxm/forkJoin/CountTaskTmpl.java
+++ src/main/java/com/lxm/forkJoin/CountTaskTmpl.java
@@ -1,4 +1,52 @@
 package com.lxm.forkJoin;
 
-public class Demo1 {
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+public class CountTaskTmpl extends RecursiveTask<Integer> {
+    private static final int THRESHOLD = 2;
+    private int start;
+    private int end;
+
+    public CountTaskTmpl(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected Integer compute() {
+        int sum = 0;
+        boolean canCompute = (end - start) <= THRESHOLD;
+        if (canCompute) {
+            for (int i = start; i <= end; i++)
+                sum += i;
+        } else {
+            //如果任务大于阀值，就分裂成两个子任务计算
+            int mid = (start + end) / 2;
+            CountTaskTmpl leftTask = new CountTaskTmpl(start, mid);
+            CountTaskTmpl rightTask = new CountTaskTmpl(mid + 1, end);
+
+            //执行子任务
+            leftTask.fork();
+            rightTask.fork();
+
+            //等待子任务执行完，并得到结果
+            int leftResult = (int) leftTask.join();
+            int rightResult = (int) rightTask.join();
+
+            sum = leftResult + rightResult;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        //使用ForkJoinPool来执行任务
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+
+        //生成一个计算资格，负责计算1+2+3+4
+        CountTaskTmpl task = new CountTaskTmpl(1, 4);
+
+        Integer r = forkJoinPool.invoke(task);
+        System.out.println(r);
+    }
 }
--- src/main/java/com/lxm/netty/io/http/NettyHttpServer.java
+++ src/main/java/com/lxm/netty/io/http/NettyHttpServer.java
@@ -1,4 +1,65 @@
 package com.lxm.netty.io.http;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+
 public class NettyHttpServer {
+
+    private void bind(int port) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .option(ChannelOption.SO_KEEPALIVE, true)
+                    .childHandler(new ChannelInitializer() {
+                        @Override
+                        protected void initChannel(Channel ch) throws Exception {
+                            ch.pipeline().addLast("reqDecoder", new HttpRequestDecoder());
+                            ch.pipeline().addLast("respEncoder", new HttpResponseEncoder());
+                            ch.pipeline().addLast("aggre", new HttpObjectAggregator(10 * 1024 * 1024));
+                            ch.pipeline().addLast("compressor", new HttpContentCompressor());
+                            ch.pipeline().addLast("handler", new NettyHttpServerHandler());
+                        }
+                    });
+            ChannelFuture f = bootstrap.bind(port).sync();
+            f.channel().closeFuture().sync();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+
+    private class NettyHttpServerHandler extends ChannelInboundHandlerAdapter {
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            String result = "<html><head></head><body><p>welcome</p><body></html>";
+            send(result, ctx, HttpResponseStatus.OK);
+        }
+
+        private void send(String content, ChannelHandlerContext ctx,
+                          HttpResponseStatus status) {
+            FullHttpResponse response =
+                    new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, status,
+                            Unpooled.copiedBuffer(content, CharsetUtil.UTF_8));
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE,
+                    "text/plain;charset=UTF-8");
+            ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int port = 8080;
+        new NettyHttpServer().bind(port);
+    }
 }
