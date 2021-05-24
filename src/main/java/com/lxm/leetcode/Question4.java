ddaaa24243b4239e21d838f08f7585ee3e85d533
lixiaoming <lixiaoming@izuche.com>
2021-05-24 21:48:30 +0800

    增加注释

--- src/main/java/com/lxm/leetcode/Question4.java
+++ src/main/java/com/lxm/leetcode/Question4.java
@@ -3,6 +3,36 @@ package com.lxm.leetcode;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: s = "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 示例 2:
+ *
+ * 输入: s = "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 示例 3:
+ *
+ * 输入: s = "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ * 示例 4:
+ *
+ * 输入: s = ""
+ * 输出: 0
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class Question4 {
     public int solution1(String s) {
         // 哈希集合，记录每个字符是否出现过
