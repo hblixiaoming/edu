d92c2fb27d34294a22d04f4ef39e4ea034679075
lixiaoming <lixiaoming@izuche.com>
2021-05-25 09:25:45 +0800

    回文串解法2

--- src/main/java/com/lxm/leetcode/Question5.java
+++ src/main/java/com/lxm/leetcode/Question5.java
@@ -1,5 +1,9 @@
 package com.lxm.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 /**
  * 给你一个字符串 s，找到 s 中最长的回文子串。
  * <p>
@@ -31,6 +35,7 @@ public class Question5 {
 
     /**
      * 暴力破解法从最长的子串开始依次判断是否是回文子串直到找到一个合适的为止
+     *
      * @param s
      * @return
      */
@@ -71,8 +76,42 @@ public class Question5 {
         return true;
     }
 
+
+    public String solution2(String s) {
+        if (s.length() == 1) {
+            return s;
+        }
+        Map<String, Integer> map = new HashMap<>();
+        map = handleSub(s, map);
+        return map.keySet().stream().findFirst().get();
+    }
+
+    public Map<String, Integer> handleSub(String sub, Map<String, Integer> map) {
+        if (sub.length() == 1) {
+            if (map.isEmpty()) {
+                map.put(sub, 1);
+            }
+        }
+        int mid = sub.length() / 2;
+        for (int i = mid, j = i + 1; i >= 0 && j < sub.length(); i--, j++) {
+            if (sub.charAt(i) != sub.charAt(j)) {
+                Optional<Map.Entry<String, Integer>> entry = map.entrySet().stream().findFirst();
+                if (entry.isPresent()) {
+                    int max = entry.get().getValue();
+                    if ((j - i) > max) {
+                        map.put(sub.substring(i, j), (j - i));
+                    }
+                }
+                map = handleSub(sub.substring(0, mid), map);
+                map = handleSub(sub.substring(mid, sub.length()), map);
+            }
+        }
+        return map;
+    }
+
     public static void main(String[] args) {
         Question5 question5 = new Question5();
-        System.out.println(question5.solution1("bb"));
+        //System.out.println(question5.solution1("bb"));
+        System.out.println(question5.solution2("bb"));
     }
 }
