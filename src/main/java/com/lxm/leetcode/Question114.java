d67eb3891a2534ec86dbea73e338924177e24c34
lixiaoming <lixiaoming@izuche.com>
2021-06-08 22:17:39 +0800

    二叉树问题

---
+++ src/main/java/com/lxm/leetcode/Question114.java
@@ -0,0 +1,32 @@
+package com.lxm.leetcode;
+
+/**
+ * 给你二叉树的根结点 root ，请你将它展开为一个单链表：
+ *
+ * 展开后的单链表应该同样使用 TreeNode ，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。
+ * 展开后的单链表应该与二叉树 先序遍历 顺序相同。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Question114 {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    public void flatten(TreeNode root) {
+
+    }
+}
---
+++ src/main/java/com/lxm/leetcode/Question654.java
@@ -0,0 +1,34 @@
+package com.lxm.leetcode;
+
+/**
+ * 给定一个不含重复元素的整数数组 nums 。一个以此数组直接递归构建的 最大二叉树 定义如下：
+ *
+ * 二叉树的根是数组 nums 中的最大元素。
+ * 左子树是通过数组中 最大值左边部分 递归构造出的最大二叉树。
+ * 右子树是通过数组中 最大值右边部分 递归构造出的最大二叉树。
+ * 返回有给定数组 nums 构建的 最大二叉树 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximum-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Question654 {
+    /**
+     Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return null;
+    }
+}
